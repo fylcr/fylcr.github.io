47a1e1a8fb793e0125e3d8e4620a685ff8a6ec3d
fylcr <1435727255@qq.com>
2025-06-28 14:39:22 +0800

    Site updated: 2025-06-28 14:39:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["posts/46e8/","posts/acec/"];function toRandomPost(){
+var posts=["posts/9073/","posts/46e8/","posts/acec/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
