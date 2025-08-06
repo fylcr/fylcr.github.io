b7ceaba361e01d5db709e7aedc67357b8f2e5d51
fylcr <1435727255@qq.com>
2025-08-06 15:25:43 +0800

    Site updated: 2025-08-06 15:25:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["posts/9073/","posts/c83/","posts/46e8/","posts/d689/","posts/acec/","posts/594a/","posts/6c53/"];function toRandomPost(){
+var posts=["posts/c83/","posts/9073/","posts/46e8/","posts/d689/","posts/6c53/","posts/594a/","posts/acec/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
